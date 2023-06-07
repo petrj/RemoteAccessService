4b4f79197b41cb4cc4446484a7512e43017dc0c7
Petr Janoušek <petrjanousek.net@gmail.com>
2023-06-07 20:49:48 +0200

    adding projects

---
+++ TestConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using LoggerService;
+using RemoteAccess;
+
+void OnMessageReceived(RemoteAccessMessage message)
+{
+    if (message == null)
+        return;
+
+    var senderFriendlyName = message.GetSenderFriendlyName();
+
+    if (message.command == "Hello")
+    {
+        Console.WriteLine("Hello, World!");
+    } else
+    {
+        Console.WriteLine("Unknown message");
+    }
+}
+
+var ip = "10.0.0.2";
+var port = 35598;
+var key = "abrakadabra";
+
+var remoteAccesssServer = new RemoteAccessService(new BasicLoggingService());
+remoteAccesssServer.SetConnection(ip, port, key);
+remoteAccesssServer.StartListening(OnMessageReceived, "Server");
+
+var remoteAccesssClient = new RemoteAccessService(new BasicLoggingService());
+remoteAccesssClient.SetConnection(ip, port, key);
+remoteAccesssClient.SendMessage(new RemoteAccessMessage()
+{
+     command = "Hello"
+});
